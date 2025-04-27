e1b116a082e328e6508068e4edb7c1b0b85f0c57
FAXRIDDIN-FT <fxidirboyev4@gmail.com>
2025-04-27 21:45:37 +0800

    frist commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,39 @@
-// const btnEl = document.querySelector(".btn")
     const creEl = document.querySelector("button")
     let offset = 0
+    creEl.addEventListener("click" ,()=>{
+        dec()
+    })
+    creEl.addEventListener("dblclick" ,()=>{
+        rest()
+    })
     function dec(){
         offset++
+        pEl.textContent = `${offset}`
+    }
+    function rest(){
+        offset = 0
+        pEl.textContent = `${offset}`
+    }
     const pEl = document.createElement("p")
     pEl.innerHTML = "0"
-    pEl.textContent = `${offset}`
     creEl.appendChild(pEl)
-    pEl = 0 
+    // topshiriq 2
+    const resumeEl = document.querySelector(".resume")
+    const btn2El = document.querySelector(".btn2")
+    const closebtnEl = document.querySelector(".close")
+    function showresume(){
+        resumeEl.classList.add("show")
     }
+    function hideresume(){
+        resumeEl.classList.remove("show")
+    }
+    btn2El.addEventListener("click" ,()=>{
+        showresume()
+    })
+    closebtnEl.addEventListener("click" ,()=>{
+        hideresume()
+    })
+
+ 
+
 
