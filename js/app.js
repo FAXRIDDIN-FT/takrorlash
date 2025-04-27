e8ed392474e5ef71f70222c9a1d5d6f969345651
FAXRIDDIN-FT <fxidirboyev4@gmail.com>
2025-04-28 01:45:38 +0800

    faxa

--- js/app.js
+++ js/app.js
@@ -1,39 +1,75 @@
-    const creEl = document.querySelector("button")
-    let offset = 0
-    creEl.addEventListener("click" ,()=>{
-        dec()
-    })
-    creEl.addEventListener("dblclick" ,()=>{
-        rest()
-    })
-    function dec(){
-        offset++
-        pEl.textContent = `${offset}`
-    }
-    function rest(){
-        offset = 0
-        pEl.textContent = `${offset}`
-    }
-    const pEl = document.createElement("p")
-    pEl.innerHTML = "0"
-    creEl.appendChild(pEl)
-    // topshiriq 2
-    const resumeEl = document.querySelector(".resume")
-    const btn2El = document.querySelector(".btn2")
-    const closebtnEl = document.querySelector(".close")
-    function showresume(){
-        resumeEl.classList.add("show")
-    }
-    function hideresume(){
-        resumeEl.classList.remove("show")
-    }
-    btn2El.addEventListener("click" ,()=>{
-        showresume()
-    })
-    closebtnEl.addEventListener("click" ,()=>{
-        hideresume()
-    })
+const creEl = document.querySelector("button")
+let offset = 0
+creEl.addEventListener("click" ,()=>{
+    dec()
+})
+creEl.addEventListener("dblclick" ,()=>{
+    rest()
+})
+function dec(){
+    offset++
+    pEl.textContent = `${offset}`
+}
+function rest(){
+    offset = 0
+    pEl.textContent = `${offset}`
+}
+const pEl = document.createElement("p")
+pEl.innerHTML = "0"
+creEl.appendChild(pEl)
+// topshiriq 2
+const resumeEl = document.querySelector(".resume")
+const btn2El = document.querySelector(".btn2")
+const closebtnEl = document.querySelector(".close")
+function showresume(){
+    resumeEl.classList.add("show")
+}
+function hideresume(){
+    resumeEl.classList.remove("show")
+}
+btn2El.addEventListener("click" ,()=>{
+    showresume()
+})
+closebtnEl.addEventListener("click" ,()=>{
+    hideresume()
+})
 
- 
+// topshiriq 3
 
+    const fNameEl = document.querySelector(".fname");
+    const lNameEl = document.querySelector(".lname");
+    const tbodyEl = document.querySelector("tbody")
+    const sanbitEl =document.querySelector(".sambit")
+    function sambit(){
+            let tr =document.createElement("tr")
+            tr.innerHTML=`
+            <td>${tbodyEl.children.length + 1}</td>
+            <td>${fNameEl.value}</td>
+            <td>${lNameEl.value}</td>
+            `
+            tbodyEl.appendChild(tr)
 
+            fNameEl.value =''
+            lNameEl.value =''
+        
+    }
+        sanbitEl.addEventListener("click" ,()=>{
+            sambit()
+        })
+
+        // topshiriq 4 
+            const userEl = document.querySelector(".user");
+            const passwordEl = document.querySelector(".password");
+            const sendEl =document.querySelector(".send")
+            function foo(){
+                if (userEl.value === "john32" && passwordEl.value === "123456") {
+                    return "Hush kelibsiz"
+                } else {
+                    return "Parol yoki user hato"
+                }
+            }
+       
+            sendEl.addEventListener( "click" ,()=> {
+                alert(foo())
+            })
+            
